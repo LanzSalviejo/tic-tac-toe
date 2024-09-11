9409d5ca2084d0bdc0ce99d6f572f4b51eebf7b5
LanzSalviejo <lanzsalviejo@gmail.com>
2024-09-11 09:30:45 -0700

    created board

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
-export default function Square() {
-    return <button className="square">X</button>;
+export default function Board() {
+    return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+    </>
+    );
   }
   
